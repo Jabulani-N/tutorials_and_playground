96e0df210b3fcd9f64266b56f6834ef48b5f80e3
root <root@LAPTOP-NLNOP13P>
2024-11-30 03:35:43 -0600

    need to figure out what output object type i got from reader

--- experiments/video_manipulation/vid_to_m4a.py
+++ experiments/video_manipulation/vid_to_m4a.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
+import moviepy
 from moviepy import VideoFileClip
+
 
 
 def vid_to_m4a():
     """
     most basic of video converters
     This will have the basis from which I add functionality
+    """
+    source_loc = '../../img/vid_manip/'
+    save_loc = './vid_to_m4a/outputs/'
+    vid_file = source_loc + 'example_vid.mp4'
+    m4a_file = save_loc + 'output_sound.m4a'
+
+
+    # Extract audio
+    audio_import_bufferSize = 1000
+    sound_file = moviepy.audio.io.readers.FFMPEG_AudioReader(vid_file, audio_import_bufferSize)
+    # write it to the specified location
+    fps, nbytes, output_buffersize = 25, 2, 500
+    moviepy.audio.io.ffmpeg_audiowriter.ffmpeg_audiowrite(sound_file, m4a_file, fps,
+                                                          nbytes, output_buffersize)
+
+    # Close the clip
+    sound_file.close()
+
+
+def vid_to_m4a_old():
+    """
+    OUTDATED
     """
     source_loc = '../../img/vid_manip/'
     save_loc = './vid_to_m4a/outputs/'
@@ -21,3 +45,4 @@ def vid_to_m4a():
 
     # Close the video clip
     vid_clip.close()
+
