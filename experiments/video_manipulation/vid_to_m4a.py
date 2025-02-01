ae2758662a2e7aad88c06be267cf5957b2bd294d
root <root@LAPTOP-NLNOP13P>
2025-01-31 21:16:05 -0600

    vid_to_m4a is ready to begin debugging

--- experiments/video_manipulation/vid_to_m4a.py
+++ experiments/video_manipulation/vid_to_m4a.py
@@ -20,18 +20,20 @@ def vid_to_m4a():
     vid_file = source_loc + 'example_vid.mp4'
     m4a_file = save_loc + 'output_audio.m4a'
 
+    print("varibles declared")
 
     # Extract audio
     audio_import_bufferSize = 1000
     sound_file = moviepy.audio.io.readers.FFMPEG_AudioReader(vid_file, audio_import_bufferSize)
+    print("soud extracted")
     # write it to the specified location
     fps, nbytes, output_buffersize = 25, 2, 500
     moviepy.audio.io.ffmpeg_audiowriter.ffmpeg_audiowrite(sound_file, m4a_file, fps,
                                                           nbytes, output_buffersize)
-
+    print("sound saved to file")
     # Close the clip
     sound_file.close()
-
+    print("sound flie closed")
 
 def vid_to_m4a_old():
     """
@@ -51,3 +53,5 @@ def vid_to_m4a_old():
     # Close the video clip
     vid_clip.close()
 
+if __name__ == '__main__':
+    vid_to_m4a()
