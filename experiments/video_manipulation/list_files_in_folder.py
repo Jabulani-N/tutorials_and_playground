e793d2068be6fe2c84bbc786a678db41c8898374
root <root@LAPTOP-NLNOP13P>
2025-02-06 02:12:18 -0600

    search for file in dir tests successfully
    extension search is case insentive via lower funciton. test operation prompts user. i'm proud of this lightning quick little minitask

--- experiments/video_manipulation/list_files_in_folder.py
+++ experiments/video_manipulation/list_files_in_folder.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python3
 
-def list_files(dir_address, extension=None):
+
+import os
+
+
+def list_files(dir_address="./", extension=".mp4"):
     """
     this function recieves a folder address
     returns names of all files with a given extension
 
     returns a list
     """
-    pass
+    relevant_files = []
+    try:
+        for file in os.listdir(dir_address):
+            # lower() to make extensions case insensitive
+            if file.lower().endswith(extension.lower()):
+                relevant_files.append(file)
+    except FileNotFoundError:
+        print("directory ", dir_address, " does not exist")
+    except Exception as e:
+        print("an exception occured:\n", e)
+
+    if relevant_files == []:
+        print("no files of extension '", extension, "' found in dir ",
+              dir_address)
+    return relevant_files
+
+if __name__ == '__main__':
+    # test mode
+    print("you are running a test for list_files!\
+\nyou'll be prompted to enter a dir to seach\
+\nand an extension to look for.\
+\n\nGood luck!")
+    dir_address = input("what directory do you want to search?")
+    extension = input("what extension do you want to search?")
+    relevant_files = list_files(dir_address, extension)
+    print("we looked for ", extension, " files in dir ", dir_address,
+          " and found:\n", relevant_files)
